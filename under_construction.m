56878334950a1ca8488665a487d6fdc2a0064830
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-04-15 01:51:57 +0900

    完成したと思ったら、音声がガビガビ。 勾配に特異点が存在してるプログラムになってる。おかしい。

--- under_construction.m
+++ under_construction.m
@@ -1,18 +1,19 @@
 clc;clear;
 
-step = 0.1;        %ステップサイズ
-num_iterations = 10;   %反復回数
+step = 0.1; %ステップサイズ
+rep = 20;   %反復回数
 
 [data1_vec,Fs]=audioread("combined_speech1.wav");
 [data2_vec,~]=audioread("combined_speech2.wav");
 data_mat = [data1_vec.';data2_vec.'];
 
-[~,num_data] = size(data_mat);  %num_dataは測定点の数
+[num_channel,num_data] = size(data_mat);  %num_dataは測定点の数
 
-W_mat = [1,1;1,1];
 I_mat = eye(2,2);
+W_mat = eye(2,2);
+distance_vec = 1:rep; %値は関係ないサイズが大切
 
-for i = 0:num_iterations-1
+for i = 1:rep
     E_mat = zeros(2);
     for t = 1:num_data
         y_vec = W_mat*data_mat(:,t);
@@ -20,18 +21,22 @@ for i = 0:num_iterations-1
         R = p_vec*y_vec.';
         E_mat = E_mat + R/t;
     end
-    W_mat = W_mat - step*(E_mat-I_mat)*W_mat;
+    W_mat = W_mat - step*(E_mat-I_mat)*W_mat
+    distance_vec(i) = DKL(W_mat,data_mat);
 end
 
+distance_vec.'
+plot(distance_vec)
 
-separate_mat = data_mat;   %同じサイズを作りたかった
+separate_mat = zeros(num_channel,num_data);
 for t = 1:num_data
     separate_mat(:,t) = W_mat*data_mat(:,t);
 end
 
 sample1 = separate_mat(1,:).';
-
-audiowrite("make_sample1.wav",sample1,Fs);
+audiowrite("make_voice1.wav",sample1,Fs);
+sample2 = separate_mat(2,:).';
+audiowrite("make_voice2.wav",sample2,Fs);
 
 %ラプラス分布--------------------------------
 function y = score_func(x)
@@ -46,3 +51,18 @@ function y = dif_score_func(x)
     end
 end
 %-------------------------------------------
+%擬距離の計算
+function distance = DKL(W_mat,data_mat)
+
+[num_channel,num_data] = size(data_mat);
+sum = 0;
+
+for t=1:num_data
+    for n=1:num_channel
+        sum = log(score_func(data_mat(num_channel,num_data)));
+    end
+end
+
+distance = -log(abs(det(W_mat)))-sum/num_data;
+
+end
--- zikken.m
+++ zikken.m
@@ -1,4 +1,35 @@
 clc;clear;
-[data1_vec,Fs]=audioread("combined_speech1.wav");
-[data2_vec,~]=audioread("combined_speech2.wav");
-data_matrix = [data1_vec.';data2_vec.'];
+
+A=zeros(2,2,3);
+
+for i=1:3
+    A(:,:,i)=eye(2);
+end
+
+A
+data_mat = [1,2,3,4,5,6;2,3,4,5,6,7]
+
+for i = 1:3
+    DKL(A(:,:,i),data_mat)
+end
+
+%data_matは二行、長い列のつもり
+function distance = DKL(W_mat,data_mat)
+
+[num_channel,num_data] = size(data_mat);
+sum = 0;
+
+for t=1:num_data
+    for n=1:num_channel
+        sum = log(score_func(data_mat(num_channel,num_data)));
+    end
+end
+
+distance = -log(abs(det(W_mat)))-sum/num_data;
+
+end
+
+
+function y = score_func(x)
+    y = exp(-abs(x))/2;
+end
