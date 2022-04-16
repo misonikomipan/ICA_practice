12b6cd1387bc0a015c53b07bf4833b7882c18136
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-04-16 18:35:57 +0900

    とりあえず2入力4出力完成

--- func_ICA.m
+++ func_ICA.m
@@ -1,10 +1,7 @@
-clc;clear;
+function [output11,output12,output21,output22] = func_ICA(filename1,filename2,step,rep,func_score,func_dif_score)
 
-step = 0.1; %ステップサイズ
-rep = 30;   %反復回数
-
-[data1_vec,Fs]=audioread("combined_speech1.wav");
-[data2_vec,~]=audioread("combined_speech2.wav");
+[data1_vec,Fs]=audioread(filename1);
+[data2_vec,~]=audioread(filename2);
 data_mat = [data1_vec.';data2_vec.'];
 
 [num_channel,num_data] = size(data_mat);  %num_dataは測定点の数
@@ -17,16 +14,15 @@ for i = 1:rep
     E_mat = zeros(2);
     for t = 1:num_data
         y_vec = W_mat*data_mat(:,t);
-        p_vec = arrayfun(@func_dif_laplace,data_mat(:,t));
+        p_vec = arrayfun(func_dif_score,data_mat(:,t));
         R = p_vec*y_vec.';
         E_mat = E_mat + R/t;
     end
     W_mat = W_mat - step*(E_mat-I_mat)*W_mat;
-    distance_vec(i) = DKL(W_mat,data_mat);
+    distance_vec(i) = -log(abs(det(W_mat)))-sum(log(arrayfun(func_score,data_mat)),"all")/num_data;
 end
 
-figure;
-plot(distance_vec)
+func_picture(distance_vec);
 
 separate_mat = zeros(num_channel,num_data);
 for t = 1:num_data
@@ -47,13 +43,4 @@ audiowrite("make_voice11.wav",output11,Fs);
 audiowrite("make_voice21.wav",output21,Fs);
 audiowrite("make_voice12.wav",output12,Fs);
 audiowrite("make_voice22.wav",output22,Fs);
-
-%-------------------------------------------
-%擬距離の計算
-function distance = DKL(W_mat,data_mat)
-
-[~,num_data] = size(data_mat);
-
-distance = -log(abs(det(W_mat)))-sum(log(arrayfun(@func_laplace,data_mat)),"all")/num_data;
-
 end
