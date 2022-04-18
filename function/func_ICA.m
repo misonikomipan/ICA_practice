439e9775de311531eee61814fc4d920f0cbcbfca
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-04-19 00:08:45 +0900

    Revert "a"
    This reverts commit dfc83bbb1e1852933c8d2b089d1336fa5db2b3eb.

--- function/func_ICA.m
+++ function/func_ICA.m
@@ -9,23 +9,25 @@ W_mat = eye(num_data);
 distance_vec = 1:rep; %値は関係ないサイズが大切
 
 for i = 1:rep
-    %E_mat = zeros(num_data);
+    E_mat = zeros(num_data);
     y_mat = W_mat*data_mat;
     %色々考えた結果DKLの計算はここ
-    distance_vec(i) = -log(abs(det(W_mat)))-sum(log(score_func(y_mat)),"all")/len_data;
-    %p_mat = ;
-    
-    %E_mat = arrayfun(score_func_dif,y_mat)*y_mat'/len_data; %ループの外で割り算
-    W_mat = W_mat - step*(score_func_dif(y_mat)*y_mat'/len_data-I_mat)*W_mat;
+    distance_vec(i) = -log(abs(det(W_mat)))-sum(log(arrayfun(score_func,y_mat)),"all")/len_data;
+    p_mat = arrayfun(score_func_dif,y_mat);
+    for t = 1:len_data
+        E_mat = E_mat + p_mat(:,t)*y_mat(:,t).';
+    end
+    E_mat = E_mat/len_data; %ループの外で割り算
+    W_mat = W_mat - step*(E_mat-I_mat)*W_mat;
 end
 
 func_picture(distance_vec);
 
 %breakupに分離信号を入れる
-%breakup_mat = zeros(num_data,len_data);
-%for t = 1:len_data
-breakup_mat = W_mat*data_mat;
-%end
+breakup_mat = zeros(num_data,len_data);
+for t = 1:len_data
+    breakup_mat(:,t) = W_mat*data_mat(:,t);
+end
 
 output_mat = breakup_mat.'/max(abs(breakup_mat),[],"all");
 
--- function/score_func_laplace_dif.m
+++ function/score_func_laplace_dif.m
@@ -1,8 +1,7 @@
 function y = dif_laplace_func(x)
-%    if x >= 0
-%        y = 1;
-%    else
-%        y = -1;
-%    end
-    y = 2*(x >= 0)-1;
+    if x >= 0
+        y = 1;
+    else
+        y = -1;
+    end
 end
