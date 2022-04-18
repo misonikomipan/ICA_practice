dfc83bbb1e1852933c8d2b089d1336fa5db2b3eb
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-04-19 00:08:30 +0900

    a

--- function/score_func_laplace_dif.m
+++ function/score_func_laplace_dif.m
@@ -1,7 +1,8 @@
 function y = dif_laplace_func(x)
-    if x >= 0
-        y = 1;
-    else
-        y = -1;
-    end
+%    if x >= 0
+%        y = 1;
+%    else
+%        y = -1;
+%    end
+    y = 2*(x >= 0)-1;
 end
