e3677da0f6573fa855f134587a4f5e2030fd6b98
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-04-19 21:06:31 +0900

    cd消滅

--- call_main.m
+++ call_main.m
@@ -4,7 +4,7 @@ clc;clear;
 
 foldername = "music";   %ICAするデータが入っているフォルダ名
 step = 0.5;             %ステップサイズ
-rep = 20;              %繰り返し回数
+rep = 20;               %繰り返し回数
 score_func = @score_func_laplace;           %優ガウス分布
 score_func_dif = @score_func_laplace_dif;   %優ガウス分布をlogとって微分
 
